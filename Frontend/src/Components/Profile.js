a8807c1dfb2c6ab646a47690b5f4a1eed41d80d3
SYN-23 <sayandas12375@gmail.com>
2023-05-22 21:39:53 +0530

    updated

--- Frontend/src/Components/Profile.js
+++ Frontend/src/Components/Profile.js
@@ -153,7 +153,7 @@ function Profile(){
 
            </div>
            <div className='image'>
-            <img src='profile background5.jpg'></img>
+            <img src='profile background5.jpg' alt=''></img>
            </div>
         </div>
     );
