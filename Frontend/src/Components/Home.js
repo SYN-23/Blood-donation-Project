11d4d1f6ebd03b15b95ea7570defb9b599bf7d6a
SYN-23 <sayandas12375@gmail.com>
2023-05-23 01:50:02 +0530

    css updated

--- Frontend/src/Components/Home.js
+++ Frontend/src/Components/Home.js
@@ -1,7 +1,9 @@
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import { useEffect } from "react";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
+import './Home.css'
 
 function Home(){
   // let navigate=useNavigate();
@@ -23,7 +25,23 @@ function Home(){
     return(
      <div>
         <Navbar></Navbar>
-       <h4>Home</h4>
+        <div className="slideImage">
+        <Carousel className="mainSlider">
+          
+                <div >
+                    <img src="image1.jpg" alt="" />
+                    <p className="legend">Donate Today Save A life</p>
+                </div>
+                <div>
+                    <img src="https://images.everydayhealth.com/images/healthy-living/10-things-doctor-wont-tell-about-blood-tests-722x406.jpg" alt="" />
+                    <p className="legend">Donate Blood Today</p>
+                </div>
+                <div>
+                    <img src="image2.jpg" alt="" />
+                    <p className="legend">You Can Help Save A Life</p>
+                </div>
+            </Carousel>
+            </div>
         
      </div>
     );
